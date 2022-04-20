300c4ee199294d7c14ab17f15c14eb374a689547
11JacobYoung11 <11JacobYoung11@gmail.com>
2022-04-19 19:45:08 -0500

    Changes
    Swapped it up

---
+++ Assets/Scripts/CraftableItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class CraftableItem : ScriptableObject
+{
+    public string Name;
+    public string Id;
+
+
+
+}
